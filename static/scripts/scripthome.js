86596b6d85807bd09cf21af4b982917d3cd969f7
aliesal12 <ali.abdulllah121@gmail.com>
2024-02-04 21:14:38 +0500

    changed localhost

--- static/scripts/scripthome.js
+++ static/scripts/scripthome.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("teacher").addEventListener("click", function() {
-        window.location.href = "http://127.0.0.1:5000/teacherbot";
+        window.location.href = "http://localhost:5000/teacherbot";
     });
 
     document.getElementById("student").addEventListener("click", function() {
-        window.location.href = "http://127.0.0.1:5000/studentbot";
+        window.location.href = "http://localhost:5000/studentbot";
     });
 });
