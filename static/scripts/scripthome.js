5bc981366c0e332c04a5765aadd8c1741a7098b0
aliesal12 <ali.abdulllah121@gmail.com>
2024-02-04 21:18:35 +0500

    fixed

--- static/scripts/scripthome.js
+++ static/scripts/scripthome.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("teacher").addEventListener("click", function() {
-        window.location.href = "https://emobot.onrender.com:5000/teacherbot";
+        window.location.href = "https://emobot.onrender.com/teacherbot";
     });
 
     document.getElementById("student").addEventListener("click", function() {
